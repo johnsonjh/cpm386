17bcca454d03ffbcbce549375894c343f1cc3a7e
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-08-02 14:00:54 -0400

    Fix typo breaking MEM
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- mem.c
+++ mem.c
@@ -306,7 +306,7 @@ _start (void) /*cppcheck-suppress unusedFunction*/
   put_region ("Ring-0 stack:          ", m.kernel_end, m.lowmem_top);
   put_region ("Video & ROM shadow:    ", 0xA0000UL, 0x100000UL);
 
-  if (base == m.tpa_base)
+  if (base != m.tpa_base)
     {
       puts ("\r\nERROR: Detected TPA base ");
       puthex32(base);
